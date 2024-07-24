e0c213b3d5758f85a6c6bddc6730ee4786d8f8dd
srinathLN7 <srinath0393@gmail.com>
2024-07-24 13:11:55 +0200

    add more code snippet for borrow ex

--- ownership/borrow/main.rs
+++ ownership/borrow/main.rs
@@ -49,18 +49,26 @@ fn main() {
     // No error this time! x still owns the String.
     println!("Owned string: {x}");
 
+     // Mutuable (Exclusive) references: i.e. Simulataneously only one mutuable references can 
+     let mut x = "Hello!".to_string(); 
+
+     let r1 = &mut x ;
+     r1.pop();
+     r1.push_str(", world");
+ 
+     println!("Modified via r1 {}", r1); 
+     // End of implicit (no open-close brackets) scope for 1st mutable borrow,
+     // b/c not used again in this function
+
+     let r2 = &mut x ;
+     r2.push_str("!"); 
+     println!("Modified via r2 {}", r2);
+
     let bash = Proc::new("bash", State::Running, Vec::new());
-
     let rsyslogd = Proc::new("rsyslogd", State::Running, vec![&bash]);
-
-
     dbg!(&bash); 
-
     let cron = Proc::new("cron", State::Running, vec![]);
-
     let init = Proc::new("init", State::Running, vec![&rsyslogd, &cron]); 
-
     dbg!(&cron);
-
     dbg!(init);   
 }
